5187960348d40c131d7cd7d739e78116bd13d6ab
Kirill Zonov <graffzon@gmail.com>
2012-08-04 17:50:23 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HijabFard::Application.config.secret_token = 'f6f191ba1e16079b7032521568b6a09c903a836c39b7a93ed5e472d1f2901d1e59503951f7cda9bf20a8972405b53e625d08e2f9de10f204c2f7cae8a437322d'
